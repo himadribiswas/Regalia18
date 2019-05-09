7a5903d497e680684cef1e65bfff4e04b4e43c4e
Himadri <himadri.fun@gmail.com>
2019-05-09 21:53:20 +0530

    First commit

---
+++ gallery.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Regalia 18 | RCCIIT steps to an Octave of Happiness </title>
+	<!-- Meta-tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta property="og:title" content="Regalia 18 - RCCIIT steps to an Octave of Happiness on 12th 13th 14th April">
+    <meta property="og:image" content="https://regalia.rccfests.in/images/fb_img.jpeg">
+    <meta property="og:url" content="https://regalia.rccfests.in/">
+    <meta property="og:site_name" content="Regalia 18 - An Octave of Happiness">
+    <meta property="og:type" content="website">
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- //Meta-tags -->
+	<!--Bootstrap-css-->
+		<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<!--//Bootstrap-css-->
+	<!--Font-awesome-css-->
+		<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+	<!--//Font-awesome-css-->
+	<!--Animation-effects-css-->
+		<link href='css/aos.css' rel='stylesheet prefetch' type="text/css" media="all" />
+	<!--//Animation-effects-css-->
+	<!--Required-css-->
+		<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<!--//Required-css-->
+	<!--fonts-->
+	<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Raleway:300,400,500,600,800" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Montez" rel="stylesheet">
+	<!--//fonts-->
+	<link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" />
+	
+	    
+
+	<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+	<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/unitegallery/1.7.40/js/unitegallery.min.js'></script> 
+		
+	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/unitegallery/1.7.40/css/unite-gallery.min.css' type='text/css' /> 
+	<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/unitegallery/1.7.40/themes/tiles/ug-theme-tiles.min.js'></script> 
+	
+</head>
+<body>
+<div class="header" id="home">
+		<!--Top-Bar-->
+		<div class="top-bar" >
+			<div class="container-fluid">
+				<div class="header-nav">
+					<nav class="navbar navbar-default">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							<h1><a class="navbar-brand" href="index.html"><span>R</span><font color="#bf2761">E</font><font color="#fbaf1b">G</font><font color="#973074">A</font><font color="#bf2761">L</font><font color="#f47735">I</font><font color="#00a8dd">A</font><font color="#bf2761"> 2K18</font></a></h1>
+						</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" style="margin-left: 0% !important" id="bs-example-navbar-collapse-1">
+							<nav class="cl-effect-15" id="cl-effect-15">
+								<ul>
+									<li><a href="index.html#home" class="" data-hover="Home">Home</a></li>
+									<li><a href="index.html#about" class="scroll" data-hover="About">About</a></li>
+									<li><a href="index.html#events" class="scroll" data-hover="Pronites">Pronites</a></li>	
+									<li><a href="index.html#prices" class="scroll" data-hover="Events">Events</a></li>
+									<li><a href="gallery.php" class="active" data-hover="Gallery">Gallery</a></li>
+									<li><a href="team.html" class="" data-hover="Team">Team</a></li>
+									<li><a href="index.html#contact" class="scroll" data-hover="Contact">Contact</a></li>
+								</ul>
+							</nav>
+						</div>
+						<div class="contact-num-w3layouts text-right">
+							<p><a href="https://www.facebook.com/regalia.rccfests/"><span class="fa fa-facebook-square" aria-hidden="true"></span></a></p>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+<div class="container">
+	<div id="gallery"  style="display:none;">
+		
+			
+			<?php
+		        require_once 'functions.php';
+		        $galImg = getImg('gallery_img');
+		        foreach($galImg as $img){
+		      ?>
+		    	<img alt="Regalia Gallery Section" src="images/gallery_img/<?= $img ?>">
+		    <?php } ?>
+		</div>
+</div>
+		<script type="text/javascript"> 
+			
+			jQuery(document).ready(function(){ 
+				jQuery("#gallery").unitegallery(); 
+			}); 
+		
+		</script>
+		<div class="copy-w3layouts">
+		<p>© 2018 Regalia | RCCIIT. All Rights Reserved | Web Designed By <a href="">Himadri Biswas </a> & <a href="">Gunjan Bhowmick </a> </p>
+	</div>
+</body>
+</html>
